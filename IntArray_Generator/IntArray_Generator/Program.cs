b1da43de3cc26896e9723d48767ca3e11986e75d
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-05-06 12:24:08 +0200

    added path variable and input

--- IntArray_Generator/IntArray_Generator/Program.cs
+++ IntArray_Generator/IntArray_Generator/Program.cs
@@ -6,7 +6,16 @@ namespace IntArray_Generator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // This console application generates an array of integers and writes them to a file.
+            // The length of that array can be set by the user after running the program.
+
+            Console.WriteLine("Name your file. Please include the path infront of the name:");
+            string path = @"" + Console.ReadLine();
+
+
+            
+            
+            Console.ReadKey();
         }
     }
 }
