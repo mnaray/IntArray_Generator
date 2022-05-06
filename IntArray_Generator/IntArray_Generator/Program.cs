364f78f65c5378710b904c3f4c75c8ce15aeeeba
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-05-06 20:48:11 +0200

    added snippet for length input

--- IntArray_Generator/IntArray_Generator/Program.cs
+++ IntArray_Generator/IntArray_Generator/Program.cs
@@ -13,6 +13,25 @@ namespace IntArray_Generator
 
             Console.WriteLine("Name your file. Please include the path infront of the name:");
             string path = @"" + Console.ReadLine();
+            path = @"C:\Users\marci\Desktop\testfile.txt";
+            int arrLength;
+
+            Console.WriteLine("Please enter the amount of integers to generate:");
+            while (true)
+            {
+                try
+                {
+                    arrLength = Convert.ToInt32(Console.ReadLine());
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("The number you entered can not be used as the amount of numbers to generate.\nPlease enter an integer above the value of zero.");
+                }
+            }
+
+
+
 
             try
             {
