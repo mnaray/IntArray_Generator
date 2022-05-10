fca6d43957250ff1693728296d397fa8153c65e1
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-05-10 11:31:32 +0200

    initial commit for secondary project

---
+++ setNum_IntArray_Generator/setNum_IntArray_Generator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace setNum_IntArray_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
