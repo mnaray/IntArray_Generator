1f54d44a55b5220f09ee78dff52aac703dc3e636
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-05-10 11:39:04 +0200

    added uinput for solution

--- setNum_IntArray_Generator/setNum_IntArray_Generator/Program.cs
+++ setNum_IntArray_Generator/setNum_IntArray_Generator/Program.cs
@@ -6,7 +6,32 @@ namespace setNum_IntArray_Generator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int solution;
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter the positive integer you would like to be the solution.");
+                    solution = Int32.Parse(Console.ReadLine());
+
+                    if (solution < 1)
+                    {
+                        throw new Exception();
+                    }
+
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Please enter a positive integer!");
+                }
+            }
+
+
+            // Lets the user know, that all of the numbers have been generated.
+            Console.WriteLine("The list of numbers has been created.\nPress any button to close the console window.");
+            Console.ReadKey();
         }
     }
 }
